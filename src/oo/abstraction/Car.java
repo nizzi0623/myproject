993f9be796670a5f92e2db593c4760fdf9facd2f
nizzi0623 <julie870529@gmail.com>
2017-03-07 16:14:18 +0800

    The progress of abstraction

---
+++ src/oo/abstraction/Car.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class Car {
+	String brand;
+	int cc;
+	String name;
+	String type;
+
+}
---
+++ src/oo/abstraction/CarTester.java
@@ -0,0 +1,11 @@
+package oo.abstraction;
+
+public class CarTester {
+
+	public static void main(String[] args) {
+		
+		Car c1 = new Car();
+		
+	}
+
+}
