82788fde59abe9cdd3dfa75c9f56756842888415
nizzi0623 <julie870529@gmail.com>
2017-03-12 22:59:20 +0800

    The progress of abstraction(2)

---
+++ src/oo/abstraction/Cosmetic.java
@@ -0,0 +1,16 @@
+package oo.abstraction;
+
+public class Cosmetic {
+	String brand;
+	String category;
+	String name;
+	String characteristic;
+	String color;
+	String packing;
+	String ingredient;
+	int g;
+	int ml;
+	
+	
+
+}
---
+++ src/oo/abstraction/CosmeticTester.java
@@ -0,0 +1,11 @@
+package oo.abstraction;
+
+public class CosmeticTester {
+
+	public static void main(String[] args) {
+		
+		Cosmetic a1 = new Cosmetic ();
+
+	}
+
+}
