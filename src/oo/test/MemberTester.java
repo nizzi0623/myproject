7479fdf330d8bfc0df0fc64fd5ffd50bf66b5a26
nizzi0623 <julie870529@gmail.com>
2017-06-13 15:31:34 +0800

    member constructor

---
+++ src/oo/test/MemberTester.java
@@ -0,0 +1,13 @@
+package oo.test;
+
+public class MemberTester {
+
+	public static void main(String[] args) {
+		Member member = new Member();
+		member.setAge(-5);
+		System.out.println(member.getAge());
+		
+		PhoneMember member2 = new PhoneMember();
+		}
+
+}
